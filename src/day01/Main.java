d16c39ea55ef5f22e219a634169c7af89b48ad08
ieui8121 <ieui8121@gmail.com>
2025-05-13 21:46:12 +0900

    init: first commit

--- src/day01/Main.java
+++ src/day01/Main.java
@@ -3,6 +3,7 @@ package day01;
 public class Main {
     public static void main(String[] args) {
        System.out.println("hello");
+       System.out.println("would");
         //주석처리 인식(실행)x
 
     }

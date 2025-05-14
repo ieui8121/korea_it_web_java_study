eda5fd7be7b91648eec0ca8c85c26aad147ed616
ieui8121 <ieui8121@gmail.com>
2025-05-14 21:06:07 +0900

    Feat: DataTypes

--- src/_01_intro/intro.java
+++ src/_01_intro/intro.java
@@ -1,9 +1,10 @@
-package day01;
+package _01_intro;
 
-public class Main {
+public class intro {
     public static void main(String[] args) {
        System.out.println("hello");
        System.out.println("would");
+        System.out.println("test");
         //주석처리 인식(실행)x
         //test
     }

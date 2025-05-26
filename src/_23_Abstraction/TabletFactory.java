17ef03f818d8e5ba916192b033767997142f7313
ieui8121 <ieui8121@gmail.com>
2025-05-26 21:27:14 +0900

    Feat: commit

---
+++ src/_23_Abstraction/TabletFactory.java
@@ -0,0 +1,11 @@
+package _23_Abstraction;
+
+//Factory라는 추상클래스 상속
+//생성자 만들어서 이름 바로 넣도록
+//재정의 다 해주고
+//고유 메소드 upgrade(String model) => 아이패드 7세대로 업그레이드 되었습ㄴ니다
+//메인에서 produce ("아이패드 6세대)
+//upgrade("아이패드 7세대")
+public class TabletFactory {
+
+}

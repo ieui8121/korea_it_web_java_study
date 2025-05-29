2b14dd1c5774d832ac460cb0d1ba05c616eef8e7
ieui8121 <ieui8121@gmail.com>
2025-05-29 19:46:48 +0900

    Feat:commit

---
+++ src/main/java/_27_Bean/Main.java
@@ -0,0 +1,19 @@
+package _27_Bean;
+
+public class Main {
+    public static void main(String[] args) {
+        //UserEntity
+        //UserId(int), username, age(int), email => private
+        //기본생성자 하나 만들기
+        //getter / setter
+
+        //toString 오버라이드해서 정보 모두 출력되도록
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserId(1);
+        userEntity.setUsername("이동윤");
+        userEntity.setAge(27);
+        userEntity.setEmail("dongyoon7212@naver.com");
+
+        System.out.println(userEntity);
+    }
+}

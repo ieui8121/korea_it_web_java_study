132f6de9337b7015067f22ebb28e362880c0bcbb
ieui8121 <ieui8121@gmail.com>
2025-05-29 18:49:40 +0900

    Feat:commit

---
+++ src/_24_interface/main.java
@@ -0,0 +1,20 @@
+package _24_interface;
+
+public class Main {
+    public static void main(String[] args) {
+        TvRemoteController tvRemoteController = new TvRemoteController(
+                new PowerButton(),
+                new ChannelUpButton(),
+                new ChannelDownButton(),
+                new VolumeUpButton(),
+                new VolumeDownButton()
+        );
+        tvRemoteController.onPressedPowerButton();
+        tvRemoteController.onPressedPowerButton();
+
+        tvRemoteController.onPressedChannelUpButton();
+        tvRemoteController.onDownVolumeDownButton();
+        tvRemoteController.onDownChannelDownButton();
+
+    }
+}

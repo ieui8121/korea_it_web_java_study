7a37cd9c5e4d379e41fe8505664b1874c9842cfa
ieui8121 <ieui8121@gmail.com>
2025-06-09 19:33:46 +0900

    Feat:commit

---
+++ src/main/java/_34_Exception/Exception.java
@@ -0,0 +1,15 @@
+package _34_Exception;
+
+public class Exception {
+    public Exception(String message) {
+    }
+
+    public static void main(String[] args) {
+        //예외가 발생했을때 처리할 코드
+        //} finally {
+        //예외가 발생을 하던 말던 무조건 실행되는 코드
+        //}
+
+
+    }
+}
